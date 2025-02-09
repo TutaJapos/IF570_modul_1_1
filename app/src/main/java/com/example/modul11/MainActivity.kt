4907f9ebaac5bfa74a5951dcb4224344a47aeb04
TutaJapos <tthekidzz@gmail.com>
2025-02-10 01:00:18 +0700

    Getting started with Compose

--- app/src/main/java/com/example/modul11/MainActivity.kt
+++ app/src/main/java/com/example/modul11/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.modul11.ui.theme.Modul11Theme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,10 +41,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Text preview")
 @Composable
 fun GreetingPreview() {
     Modul11Theme {
-        Greeting("Android")
+        Greeting(name = "Android")
     }
 }
